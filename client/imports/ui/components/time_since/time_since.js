401b25b19183d38f494c3a89dae6c113764d3aee
Dan Rosart <Torgen@users.noreply.github.com>
2023-06-17 00:47:48 -0700

    Lint

--- client/imports/ui/components/time_since/time_since.js
+++ client/imports/ui/components/time_since/time_since.js
@@ -1,7 +1,7 @@
-import "./time_since.html"
+import "./time_since.html";
 
 Template.time_since.helpers({
-  since: function() {
+  since() {
     return ((Session.get("currentTime") || Date.now()) - this.timestamp) / 1000;
-  }
+  },
 });
