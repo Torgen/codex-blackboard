0e5c021dd036a6aad39ced92702f4c95dd53320b
Dan Rosart <Torgen@users.noreply.github.com>
2024-01-15 14:22:19 -0800

    Tests

--- client/imports/ui/pages/statistics/puzzle_feed.test.js
+++ client/imports/ui/pages/statistics/puzzle_feed.test.js
@@ -8,21 +8,21 @@ describe("puzzle feed", () =>
     const p = new PuzzleFeed("timestamp", callback);
     p.addedAt({ timestamp: 1000 }, 0);
     p.addedAt({ timestamp: 2000 }, 1);
-    p.addedAt({ timestamp: 1500 }, 1);
+    p.addedAt({ timestamp: 1500, puzzles: [] }, 1);
     assert.isTrue(callback.calledThrice, "initial");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
     ]);
     callback.resetHistory();
     Session.set("currentTime", 2500);
     p.updateNow();
     assert.isTrue(callback.calledOnce, "set now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
       { x: 2500, y: 3 },
     ]);
     callback.resetHistory();
@@ -30,51 +30,51 @@ describe("puzzle feed", () =>
     p.updateNow();
     assert.isTrue(callback.calledOnce, "update now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
       { x: 3000, y: 3 },
     ]);
     callback.resetHistory();
     p.addedAt({ timestamp: 2500 }, 3);
     assert.isTrue(callback.calledOnce, "insert before now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
-      { x: 2500, y: 4 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
+      { x: 2500, y: 4, isMeta: false },
       { x: 3000, y: 4 },
     ]);
     callback.resetHistory();
     p.addedAt({ timestamp: 3500 }, 4);
     assert.isTrue(callback.calledOnce, "append after now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
-      { x: 2500, y: 4 },
-      { x: 3500, y: 5 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
+      { x: 2500, y: 4, isMeta: false },
+      { x: 3500, y: 5, isMeta: false },
     ]);
     callback.resetHistory();
     Session.set("currentTime", 4000);
     p.updateNow();
-    p.changedAt({ timestamp: 4500 }, { timestamp: 3500 }, 4);
+    p.changedAt({ timestamp: 4500, puzzles: [] }, { timestamp: 3500 }, 4);
     assert.isTrue(callback.calledTwice, "move after now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
-      { x: 2500, y: 4 },
-      { x: 4500, y: 5 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
+      { x: 2500, y: 4, isMeta: false },
+      { x: 4500, y: 5, isMeta: true },
     ]);
     callback.resetHistory();
     p.removedAt({ timestamp: 4500 }, 4);
     assert.isTrue(callback.calledOnce, "delete after now");
     assert.deepEqual(p.data, [
-      { x: 1000, y: 1 },
-      { x: 1500, y: 2 },
-      { x: 2000, y: 3 },
-      { x: 2500, y: 4 },
+      { x: 1000, y: 1, isMeta: false },
+      { x: 1500, y: 2, isMeta: true },
+      { x: 2000, y: 3, isMeta: false },
+      { x: 2500, y: 4, isMeta: false },
       { x: 4000, y: 4 },
     ]);
   }));
