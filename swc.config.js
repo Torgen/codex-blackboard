d96b506869ac631beb3e90857be3ceed430268c6
Dan Rosart <Torgen@users.noreply.github.com>
2025-06-27 23:51:00 -0700

    Format swc.config.js
    Also exclude test files themselves from coverage

--- swc.config.js
+++ swc.config.js
@@ -1,9 +1,22 @@
-module.exports = (process.env.BABEL_ENV == "COVERAGE") ? {
-  "jsc": {
-    "experimental": {
-      "plugins": [["swc-plugin-coverage-instrument", {
-        "unstableExclude": ["packages/**", "node_modules/**"]
-      }]]
-    }
-  }
-} : {}
+module.exports =
+  process.env.BABEL_ENV == "COVERAGE"
+    ? {
+        jsc: {
+          experimental: {
+            plugins: [
+              [
+                "swc-plugin-coverage-instrument",
+                {
+                  unstableExclude: [
+                    "packages/**",
+                    "node_modules/**",
+                    "**/*.app-test.js",
+                    "**/*.test.js",
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }
+    : {};
