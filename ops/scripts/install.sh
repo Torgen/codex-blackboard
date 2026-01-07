63695ce8b4f939b77eee1e38b24bb877a47b2072
Brian Chen <brian.chxn@gmail.com>
2026-01-07 00:36:55 -0800

    Simplify install.sh user/group creation
    These lines were crashing the install script for me, I think because
    useradd also tries creating the group of the same name and fails
    because it already exists. It also makes it hard to restart an install
    that crashed after these lines, since it's not idempotent.
    
    This might slightly change how the blackboard user and group are set up
    in practice from the existing code but I couldn't see where this
    matters.

--- ops/scripts/install.sh
+++ ops/scripts/install.sh
@@ -37,8 +37,9 @@ sudo apt-get install -y mongodb-org nodejs software-properties-common
 sudo npm install -g handlebars-cmd
 cd "$scriptroot"/../..
 
-sudo addgroup blackboard
-sudo useradd -G blackboard -m blackboard
+# Don't crash if user reruns the install script and we try making the same user
+# more than once
+sudo useradd -m blackboard || true
 
 # Build the bundle, then install it.
 meteor npm install
